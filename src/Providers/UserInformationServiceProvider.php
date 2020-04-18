8a4e4be0f348d334b57ebad3b24d73dcfa3c7287
hamed sh <megahamed22@yahoo.com>
2020-04-18 17:28:32 +0430

    create service provider

---
+++ src/Providers/UserInformationServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Parax\UserInformation\Providers;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class UserInformationServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
+}
